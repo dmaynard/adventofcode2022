53b4dc031c206961fdb76e61156010676cda9dd2
dmaynard <davidsmaynard@gmail.com>
2023-01-21 00:07:26 -0800

    day-05 working

--- day-05/src/lib.rs
+++ day-05/src/lib.rs
@@ -49,6 +49,31 @@ fn parse_section1(stacks: &mut Vec<VecDeque<char>>, section1: &str) -> u32 {
     }
     num_moves
 }
+fn parse_section1_part2(stacks: &mut Vec<VecDeque<char>>, section1: &str) -> u32 {
+    let mut num_moves = 0;
+    for line in section1.split("\n") {
+        let mut params: Vec<usize> = Vec::new();
+        // println!("line {}", line);
+        for token in line.split_whitespace() {
+            // println!(" token {}", token);
+            match token.parse() {
+                Ok(val) => params.push(val),
+                Err(_e) => {}
+            }
+        }
+        let mut temp_stack: VecDeque<char> = VecDeque::new();
+        for _i in 0..params[0] {
+            temp_stack.push_front(stacks[params[1] - 1].pop_front().unwrap());
+        }
+        for _i in 0..params[0] {
+            stacks[params[2] - 1].push_front(temp_stack.pop_front().unwrap());
+        }
+        // dbg!(&stacks[0]);
+        // dbg!(&stacks[1]);
+        // dbg!(&stacks[2]);
+    }
+    num_moves
+}
 pub fn process_part1(input: &str, width: u32) -> String {
     let mut stacks: Vec<VecDeque<char>> = Vec::new();
 
@@ -92,40 +117,48 @@ pub fn process_part1(input: &str, width: u32) -> String {
     }
     answer
 }
-pub fn process_part2(input: &str) -> String {
-    let result: i32 = input
-        .split("\n")
-        .map(|line| {
-            // println!("line {}", line);
-            let mut limits: Vec<i32> = Vec::new();
-            line.split(",")
-                .map(|range| {
-                    // println!(" range {}", range);
-                    range
-                        .split("-")
-                        .map(|bound| {
-                            // println!(" bound {}", bound);
-                            match bound.parse::<i32>() {
-                                Ok(val) => limits.push(val),
-                                Err(_e) => limits.push(0),
-                            }
-                            1
-                        })
-                        .sum::<i32>()
-                })
-                .sum::<i32>();
-            if (limits[2] >= limits[0] && limits[2] <= limits[1])
-                || (limits[3] >= limits[0] && limits[3] <= limits[1])
-            {
-                // println!(" found one {} ", line);
-                1
-            } else {
-                0
-            }
-        })
-        .sum();
+pub fn process_part2(input: &str, width: usize) -> String {
+    let mut stacks: Vec<VecDeque<char>> = Vec::new();
 
-    result.to_string()
+    for _i in 0..width {
+        // println!(" pushing VecDeque");
+        stacks.push(VecDeque::new());
+    }
+    //println!(" stacks.len () {}", stacks.len());
+    let mut section_num = 0;
+
+    for section in input.split("\n\n") {
+        // println!(" section {}", section);
+        if section_num == 0 {
+            let num_boxes = parse_section0(&mut stacks, section);
+            println!("num_boxes {}", num_boxes);
+            // dbg!(&stacks[0]);
+            // dbg!(&stacks[1]);
+            // dbg!(&stacks[2]);
+        }
+        if section_num == 1 {
+            let num_moves = parse_section1_part2(&mut stacks, section);
+            // println!("num_moves {}", num_moves);
+            // dbg!(&stacks[0]);
+            // dbg!(&stacks[1]);
+            // dbg!(&stacks[2]);
+        }
+        section_num = section_num + 1;
+    }
+
+    // dbg!(&stacks[0]);
+    // dbg!(&stacks[1]);
+    // dbg!(&stacks[2]);
+    let mut answer = String::new();
+
+    for mut stack in stacks {
+        dbg!(&stack);
+        match &stack.pop_front() {
+            Some(char) => answer.push(*char),
+            None => {}
+        }
+    }
+    answer
 }
 
 #[cfg(test)]
@@ -143,6 +176,7 @@ move 2 from 2 to 1
 move 1 from 1 to 2";
 
     #[test]
+    #[ignore]
 
     fn it_works() {
         let result = process_part1(INPUT, 3);
@@ -150,9 +184,9 @@ move 1 from 1 to 2";
     }
 
     #[test]
-    #[ignore]
+
     fn part2_works() {
-        let result = process_part2(INPUT);
-        assert_eq!(result, "4");
+        let result = process_part2(INPUT, 3);
+        assert_eq!(result, "MCD");
     }
 }
