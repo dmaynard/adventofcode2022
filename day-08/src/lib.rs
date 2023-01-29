ef719834876d47837578c6e1494d276f749c0920
dmaynard <davidsmaynard@gmail.com>
2023-01-28 18:07:23 -0800

    day08 complete

--- day-08/src/lib.rs
+++ day-08/src/lib.rs
@@ -39,14 +39,67 @@ pub fn is_visible(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> bool {
 
     visible_from_left || visible_from_right || visible_from_top || visible_from_bottom
 }
+pub fn view(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> u32 {
+    let my_val = grid[y][x];
 
+    // from left
+    let visible_to_left = {
+        let mut visible = 0;
+        for i in (0..x).rev() {
+            visible = visible + 1;
+            if grid[y][i] >= my_val {
+                break;
+            }
+        }
+        visible
+    };
+    let visible_to_right = {
+        let mut visible = 0;
+        for i in (x + 1)..grid[y].len() {
+            visible = visible + 1;
+            if grid[y][i] >= my_val {
+                break;
+            }
+        }
+        visible
+    };
+    let visible_to_top = {
+        let mut visible = 0;
+        for j in (0..y).rev() {
+            visible = visible + 1;
+            if grid[j][x] >= my_val {
+                break;
+            }
+        }
+        visible
+    };
+    let visible_to_bottom = {
+        let mut visible = 0;
+        for j in (y + 1)..grid.len() {
+            visible = visible + 1;
+            if grid[j][x] >= my_val {
+                break;
+            }
+        }
+        visible
+    };
+    // println!(
+    //     "{}, {},{}, {} {} {} {}",
+    //     my_val, y, x, visible_to_left, visible_to_right, visible_to_top, visible_to_bottom
+    // );
+    if x == 0 || y == 0 || x == grid[y].len() - 1 || y == grid.len() - 1 {
+        0
+    } else {
+        visible_to_left * visible_to_right * visible_to_top * visible_to_bottom
+    }
+}
 pub fn process_part1(input: &str) -> String {
     let mut grid: Vec<Vec<usize>> = Vec::new();
 
     for line in input.lines() {
         grid.push(line.as_bytes().iter().map(|b| *b as usize - 48).collect());
     }
-    let test = is_visible(&grid, 0, 0);
+
     let max_height = grid.len() - 1;
     let mut visible_trees: Vec<Vec<bool>> = grid
         .iter()
@@ -65,12 +118,6 @@ pub fn process_part1(input: &str) -> String {
                 .collect()
         })
         .collect();
-    println!(
-        " width {} height {} ",
-        visible_trees[0].len(),
-        visible_trees.len()
-    );
-
     for y in 0..grid.len() {
         for x in 0..grid[y].len() {
             visible_trees[y][x] = is_visible(&grid, x, y);
@@ -86,19 +133,21 @@ pub fn process_part1(input: &str) -> String {
         .to_string()
 }
 pub fn process_part2(input: &str) -> String {
-    let mut result = input
-        .split("\n\n")
-        .map(|elf_load| {
-            elf_load
-                .lines()
-                .map(|item| item.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .collect::<Vec<_>>();
-    result.sort_by(|a, b| b.cmp(a));
-    // dbg!(result);
-    let sum: u32 = result.iter().take(3).sum();
-    sum.to_string()
+    let mut grid: Vec<Vec<usize>> = Vec::new();
+
+    for line in input.lines() {
+        grid.push(line.as_bytes().iter().map(|b| *b as usize - 48).collect());
+    }
+    let mut max_view = 0;
+    for (y, row) in grid.iter().enumerate() {
+        for (x, _cell) in row.iter().enumerate() {
+            let this_view = view(&grid, x, y);
+            if this_view >= max_view {
+                max_view = this_view
+            }
+        }
+    }
+    max_view.to_string()
 }
 #[cfg(test)]
 mod tests {
@@ -111,15 +160,15 @@ mod tests {
 35390";
 
     #[test]
+
     fn it_works() {
         let result = process_part1(INPUT);
         assert_eq!(result, "21");
     }
 
     #[test]
-    #[ignore]
     fn part2_works() {
         let result = process_part2(INPUT);
-        assert_eq!(result, "45000");
+        assert_eq!(result, "8");
     }
 }
