2022d45a69e582750eaa82336768dec10bbc4a36
dmaynard <davidsmaynard@gmail.com>
2023-02-05 12:55:36 -0800

    part-2 day-09 working

--- day-09/src/lib.rs
+++ day-09/src/lib.rs
@@ -30,7 +30,7 @@ fn make_move(b: &mut Board, d: Direction) -> Point {
         Direction::L => b.head.x = b.head.x - 1,
         Direction::R => b.head.x = b.head.x + 1,
     }
-    let delta: (i32, i32) = if b.are_touching() {
+    let delta: (i32, i32) = if are_touching(b.head, b.tail) {
         (0, 0)
     } else {
         match (b.tail.x - b.head.x, b.tail.y - b.head.y) {
@@ -82,11 +82,80 @@ pub fn process_part1(input: &str) -> String {
 
     visited.len().to_string()
 }
-pub fn process_part2(input: &str) -> String {
-    "42".to_string()
 
-    //vmax.unwrap().unwrap().to_string()
-    //  max_view.to_string()
+fn are_touching(p1: Point, p2: Point) -> bool {
+    ((p1.x - p2.x).abs() < 2) && ((p1.y - p2.y).abs() < 2)
+}
+fn make_long_move(l: &mut [Point; 10], d: Direction) -> Point {
+    match d {
+        Direction::U => l[0].y = l[0].y + 1,
+        Direction::D => l[0].y = l[0].y - 1,
+        Direction::L => l[0].x = l[0].x - 1,
+        Direction::R => l[0].x = l[0].x + 1,
+    }
+    for i in 1..l.len() {
+        let diff = (l[i].x - l[i - 1].x, l[i].y - l[i - 1].y);
+        let delta: (i32, i32) = if are_touching(l[i - 1], l[i]) {
+            (0, 0)
+        } else {
+            match diff {
+                (2, 0) => (-1, 0),
+                (-2, 0) => (1, 0),
+                (0, 2) => (0, -1),
+                (0, -2) => (0, 1),
+                // diagonal cases right side
+                (1, 2) => (-1, -1),
+                (2, 1) => (-1, -1),
+                (2, -1) => (-1, 1),
+                (1, -2) => (-1, 1),
+                // diagonal cases left side
+                (-1, 2) => (1, -1),
+                (-2, 1) => (1, -1),
+                (-2, -1) => (1, 1),
+                (-1, -2) => (1, 1),
+                (-2, -2) => (1, 1),
+                (-2, 2) => (1, -1),
+                (2, -2) => (-1, 1),
+                (2, 2) => (-1, -1),
+
+                (_, _) => {
+                    println!("impossible board on rung {} {:?} ", i, l);
+                    println!(" diff {:?}", diff);
+                    (0, 0)
+                }
+            }
+        };
+
+        l[i].x = l[i].x + delta.0;
+        l[i].y = l[i].y + delta.1;
+
+        if delta.0 != 0 || delta.1 != 0 {
+            // println!("moved ladder {} {:?}", i, delta);
+        }
+    }
+    l[l.len() - 1]
+}
+
+pub fn process_part2(input: &str) -> String {
+    let mut visited: HashSet<Point> = HashSet::new();
+
+    let mut ladder: [Point; 10] = [Point { x: 0, y: 0 }; 10];
+    visited.insert(ladder[ladder.len() - 1]);
+    for line in input.lines() {
+        let d = match &line[0..1] {
+            "U" => Direction::U,
+            "D" => Direction::D,
+            "L" => Direction::L,
+            "R" => Direction::R,
+            _ => panic!("unknown direction"),
+        };
+        let times: u32 = line[2..line.len()].parse().unwrap();
+        for _i in 1..=times {
+            visited.insert(make_long_move(&mut ladder, d));
+        }
+    }
+
+    visited.len().to_string()
 }
 #[cfg(test)]
 mod tests {
@@ -140,8 +209,24 @@ R 2";
     }
 
     #[test]
-    #[ignore]
+
     fn part2_works() {
+        let mut ladder: [Point; 10] = [Point { x: 0, y: 0 }; 10];
+        let result = process_part2(INPUT);
+        println!("processed input for Part-2 answer was {}", result);
+
+        for i in 0..20 {
+            let d = if i % 2 == 0 {
+                Direction::U
+            } else {
+                Direction::R
+            };
+            // !(" move {}  d {:?}", i, d);
+
+            let pu = make_long_move(&mut ladder, d);
+            // println!(" {:?}", ladder);
+            // println!("tail {:?}", pu)
+        }
         let result = process_part2(INPUT);
         assert_eq!(result, "8");
     }
